1ed55907d01be02bc74fb0e2e2e5cfecee8742fc
santiagoreyb <screyb@javeriana.edu.co>
2023-09-03 21:24:00 -0500

    Primer commit

---
+++ src/main/java/com/adminParking/adminParking/AdminParkingApplication.java
@@ -0,0 +1,13 @@
+package com.adminParking.adminParking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminParkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminParkingApplication.class, args);
+	}
+
+}
