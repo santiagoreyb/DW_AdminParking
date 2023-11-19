287cec3a72231a39d71d01490f71634b88f51507
Kevin López <kevinleonardolopez@gmail.com>
2023-11-19 18:46:53 -0500

    RESTTIPOVEHICULO

--- src/main/java/com/adminParking/adminParking/controller/PisoRestController.java
+++ src/main/java/com/adminParking/adminParking/controller/PisoRestController.java
@@ -17,7 +17,6 @@ public class PisoRestController {
     @Autowired
     PisoRepository pisoRepository;
 
-
     @Autowired 
     VehiculoRepository vehiculoRepository;
     @GetMapping("/getPisos")
--- src/main/java/com/adminParking/adminParking/controller/TipoVehiculoRestController.java
+++ src/main/java/com/adminParking/adminParking/controller/TipoVehiculoRestController.java
@@ -1,21 +1,25 @@
 package com.adminParking.adminParking.controller;
 
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import com.adminParking.adminParking.model.PisoEntity;
 import com.adminParking.adminParking.model.TipoVehiculoEntity;
+import com.adminParking.adminParking.repositories.PisoRepository;
 import com.adminParking.adminParking.repositories.TipoVehiculoRepository;
 
 @RestController
 @RequestMapping("/tiposvehiculoRest")
 public class TipoVehiculoRestController {
-    
+
     @Autowired
-    TipoVehiculoRepository tipoVehiculoRepository ;
+    TipoVehiculoRepository tipoVehiculoRepository;
+
+    @Autowired
+    PisoRepository pisoRepository;
 
     @Secured({ "ADMIN", "PORTERO" })
     @GetMapping("/getTipos")
@@ -28,5 +32,29 @@ public class TipoVehiculoRestController {
     public TipoVehiculoEntity getTipoById(@PathVariable Long id) {
         return tipoVehiculoRepository.findById(id).orElse(null);
     }
+
+    @Secured({ "ADMIN"})
+    @PostMapping("/anadirTipoVehiculo")
+    public void createVehiculo(@RequestBody String tipo) {
+        TipoVehiculoEntity tipoV = new TipoVehiculoEntity(tipo);
+        System.out.println("Received tipo: " + tipo);
+        tipoVehiculoRepository.save(tipoV);
+    }
+
+    @Secured({ "ADMIN" })
+    @DeleteMapping("/deleteTipoVehiculo/{id}")
+    public void deleteTipo(@PathVariable Long id) {
+        System.out.println("Received id: " + id);
+        tipoVehiculoRepository.deleteById(id);
+    }
+
+    @GetMapping("/getPisosByTipoVehiculo/{id}")
+    @Secured({ "ADMIN"})
+    public List<PisoEntity> getAllPisosByTipoVehiculo(@PathVariable Long id) {
+        TipoVehiculoEntity tipoVehiculo = tipoVehiculoRepository.findById(id).orElse(null);
+        List<PisoEntity> pisosConTipo = pisoRepository.findByTipoVehiculo(tipoVehiculo);
+        return pisosConTipo;
+    }
+
 
 }
